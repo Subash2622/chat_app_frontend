bc6c88319a7c9d413578772c395b5b88eaba5350
Subash2622e <subash9938sahoo@gmail.com>
2024-12-22 23:57:54 +0530

    Complete the design part of JoinCreateChat

--- src/config/Routes.jsx
+++ src/config/Routes.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Routes, Route } from "react-router";
 import App from "../App";
+import JoinCreateChat from "../components/JoinCreateChat";
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/chat" element={<h1>This is a chat</h1>} />
+      <Route path="/joincreatechat" element={<JoinCreateChat />} />
     </Routes>
   );
 };
